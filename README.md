9f854dcb44b5f76ca5e3dd8b8bdf248cee93b4ec
mouweng <325242850@qq.com>
2022-02-24 20:46:29 +0800

    Add V3

--- README.md
+++ README.md
@@ -27,3 +27,19 @@ nc 127.0.0.1 8888
 ```shell
 nc 127.0.0.1 8888
 ```
+
+## 版本V3-用户消息广播功能
+- 构建服务
+```shell
+go build -o server main.go server.go user.go
+./server
+```
+- 测试
+```shell
+nc 127.0.0.1 8888
+hello
+```
+```shell
+nc 127.0.0.1 8888
+nihao
+```
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"io"
 )
 
 type Server struct {
@@ -51,7 +52,29 @@ func (this *Server) Handler(conn net.Conn) {
 	this.mapLock.Lock()
 	this.OnlineMap[user.Name] = user
 	this.mapLock.Unlock()
+
+	// 广播当前用户上线消息
 	this.BroadCast(user, "已上线")
+
+	// 接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+			// 提取用户消息(去除'\n')
+			msg := string(buf[:n - 1])
+			// 将消息进行广播
+			this.BroadCast(user, msg)
+		}
+	}()
 
 	select{}
 }
