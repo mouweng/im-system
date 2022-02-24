5a0b8992d1038c3585f31efdf1532ea4a8995d1a
mouweng <325242850@qq.com>
2022-02-24 21:58:01 +0800

    Add v6

--- README.md
+++ README.md
@@ -74,3 +74,18 @@ who
 nc 127.0.0.1 8888
 who
 ```
+
+
+## 版本V6-修改用户名
+- 构建服务
+```shell
+go build -o server main.go server.go user.go
+./server
+```
+- 测试
+```shell
+nc 127.0.0.1 8888
+who
+rename|yifanweng
+who
+```
--- user.go
+++ user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"strings"
 )
 
 type User struct {
@@ -64,6 +65,22 @@ func (this *User) DoMessage(msg string) {
 			this.SendMsg(OnlineMsg)
 		}
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7]== "rename|" {
+		// 消息格式 rename|张三
+		newName := strings.Split(msg, "|")[1]
+		// 判断name是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMsg("您已经更新用户名: " + this.Name + "\n")
+		}
 	} else {
 		this.server.BroadCast(this, msg)
 	}
